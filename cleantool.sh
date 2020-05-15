0c7f5a20fcdcc223233b3e46af28805d32d1d654
Jessica L. Mulein <jessica@ApertureImaging.com>
2020-05-15 13:31:56 -0700

    Testing absolute pathing

--- cleantool.sh
+++ cleantool.sh
@@ -6,18 +6,31 @@ FORCERM=0
 RECURSIVE=0
 #CLEANEXT=#DO NOT DEFINE/UNCOMMENT! Required for logic
 
+# https://stackoverflow.com/questions/4175264/how-to-retrieve-absolute-path-given-relative/51264222#51264222
+function toAbsPath {
+    local target="$1"
+
+    if [ "$target" == "." ]; then
+        echo "$(pwd)"
+    elif [ "$target" == ".." ]; then
+        echo "$(dirname "$(pwd)")"
+    else
+        echo "$(cd "$(dirname "$1")"; pwd)/$(basename "$1")"
+    fi
+}
+
 function CleanDir() {
-  OFS=$IFS
+  local OFS=$IFS
   IFS=$'\0'
 
-  DONE=()
+  local DONE=()
   for A in ./*.*.*; do
     #AA=`echo "$A" | sed -E 's/^(.*)\.(.*)\.(.*)$/\1/'`
-    AB=`echo "$A" | sed -E 's/^.+\.(.+)\..+$/\1/'`
+    local AB=`echo "$A" | sed -E 's/^.+\.(.+)\..+$/\1/'`
     #AC=`echo "$A" | sed -E 's/^(.*)\.(.*)\.(.*)$/\3/'`
     if [[ ! " ${DONE[*]} " == *"${AB}"* ]]; then
       DONE+=(${AB})
-      ISAMPM=`echo "${AB}" | grep -E "^[0-9]+ (AM|PM)$"`
+      local ISAMPM=`echo "${AB}" | grep -E "^[0-9]+ (AM|PM)$"`
       if [ "${ISAMPM}" = "" ]; then
         CleanExtension "${AB}"
       fi
@@ -27,7 +40,7 @@ function CleanDir() {
 }
 
 function RecursiveCleanDir() {
-  OFS=$IFS
+  local OFS=$IFS
   IFS=$'\0'
 
   # clean the files in this directory
@@ -46,43 +59,44 @@ function RecursiveCleanDir() {
 }
 
 function CleanExtension() {
-  OFS=$IFS
+  local OFS=$IFS
   IFS=$'\0'
-  EXT=$1
+  local EXT=$1
   if [ "$1" = "" ]; then
     exit
   fi
   if [[ "$1" = "*" ]] || [[ "$1" = "./*" ]]; then
     return
   fi
-  OPWD=`pwd`
+  local OPWD=`pwd`
+  local ABSPWD=$(toAbsPath)
 
   echo "Checking $OPWD for *.${EXT}.* duplicates"
   for A in ./*.${EXT}; do
-    AEXT=""
-    CNT=0
-    Afilename="${A%.${EXT}}"
+    local AEXT=""
+    local CNT=0
+    local Afilename="${A%.${EXT}}"
     for B in ./${Afilename}.${EXT}.*; do
-      Bextension="${B##*.}"
-      AEXT="${Afilename}.${EXT}.${Bextension}"
+      local Bextension="${B##*.}"
+      local AEXT="${Afilename}.${EXT}.${Bextension}"
       if [ "${Afilename}" != "*" -a "${Bextension}" != "*" -a "${Afilename}" != "./*" -a "${Bextension}" != "./*" ]; then
         echo "- Comparing $OPWD/${A} +=> .${Bextension}"
         if [ $USEMD5 -eq 1 ]; then
-          F1=`md5sum -b "${A}" 2>/dev/null | awk '{ print $1; }' &`
-          F2=`md5sum -b "${AEXT}" 2>/dev/null | awk '{ print $1; }' &`
-          FAIL=0
+          local F1=`md5sum -b "${A}" 2>/dev/null | awk '{ print $1; }' &`
+          local F2=`md5sum -b "${AEXT}" 2>/dev/null | awk '{ print $1; }' &`
+          local FAIL=0
           for job in `jobs -p`; do
             wait $job || let "FAIL+=1"
           done
 
           if [ "$FAIL" == "0" -a "$F1" = "$F2" -a "$F1" != "" ]; then
-            CMPMATCH=0
+            local CMPMATCH=0
           else
               echo "  - Failed joining $FAIL/2 hashes"
           fi
         else
             cmp -s "${A}" "${AEXT}" > /dev/null 2>&1
-            CMPMATCH=$?
+            local CMPMATCH=$?
         fi
 
         if [ $CMPMATCH -eq 0 ]; then
@@ -131,6 +145,9 @@ for ARG in $@; do
         ;;
     "-md5")
         USEMD5=1
+        ;;
+    "-dry-run")
+        DRYRUN=1
         ;;
     *)
         # we've now seen an extension or some other data, skip it, but now we can assume we're doing CleanExtension since that is the only one that takes an argument
